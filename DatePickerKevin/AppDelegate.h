c27bac523516a1db81e599fc23a0d02a9d089220
Kevin Kabore <kevinkabore@172-16-185-249.dynapool.nyu.edu>
2016-03-21 12:57:12 -0400

    Initial Commit

---
+++ DatePickerKevin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DatePickerKevin
+//
+//  Created by Kevin Kabore on 3/21/16.
+//  Copyright © 2016 Kevin Kabore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DatePickerKevin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DatePickerKevin
+//
+//  Created by Kevin Kabore on 3/21/16.
+//  Copyright © 2016 Kevin Kabore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
